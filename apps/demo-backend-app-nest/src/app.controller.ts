3969014c49c5905bcb4d286cc713fee557fea017
Elad Ab <abramovich.elad@gmail.com>
2024-01-28 13:15:44 +0200

    Added PriciModule

--- apps/demo-backend-app-nest/src/app.controller.ts
+++ apps/demo-backend-app-nest/src/app.controller.ts
@@ -1,23 +1,25 @@
 import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { IsAllowedGuard } from '@prici/sdk/nest';
-import { priciSdk } from './common/prici';
+import { IsAllowedGuard, PriciService } from '@prici/sdk/nest';
 
 const featureId = process.env.TODOS_FEATURE_ID as string;
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private priciService: PriciService,
+  ) {}
 
   @Get('todos')
   getAllTodos() {
+    console.log('priciService', this.priciService, this.priciService.sdk);
     return this.appService.getAllTodos();
   }
 
   @Post('todos')
   @UseGuards(
     IsAllowedGuard({
-      sdk: priciSdk,
       errorMessage: 'User is out of quota',
       getAccountId: (_) => 'demo-account',
       getFieldId: (_) => featureId,
